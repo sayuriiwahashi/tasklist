7f9c9ce1eff2c0c429647989f5385c10cc45d4a2
sayuri.iwahashi <i.s.1084333@gmail.com>
2024-04-25 22:51:18 +0900

    getAllTasksを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -12,20 +12,16 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 @Entity
 @NamedQueries({
-    @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-    )
-})
-@Table(name = "messages")
-public class Message {
+   @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+    )})
+@Table(name = "tasks")
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -44,13 +40,6 @@ public class Message {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     public String getContent() {
         return content;
